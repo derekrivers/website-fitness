a9fa6ead630ef4bd964df28f8bc6de2e9fb177ab
RedDwarf <reddwarf@local.invalid>
2026-04-20 15:59:00 +0000

    [RedDwarf] Build the single-page FitnessFormula marketing experience

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,6 +3,12 @@ require "test_helper"
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
+
     assert_response :success
+    assert_select "section", count: 7
+    assert_select "section#services"
+    assert_select "section#book"
+    assert_select "a", text: "Book a session", minimum: 1
+    assert_match "FitnessFormula", @response.body
   end
 end
